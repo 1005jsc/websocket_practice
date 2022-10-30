afdba44dcd3370a469961f1bbd8d0b652842abe3
cho_jae_sin <1005jsc@naver.com>
2022-10-31 08:56:46 +0900

    key를 눌러 서버랑 웹소켓으로 통신하기

---
+++ server.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+// const app = express();
+
+// app.use('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// app.listen(8080);
+
+const WebSocket = require('ws');
+
+// 포트 8081 로 웹소켓 연결하겠다
+const socket = new WebSocket.Server({
+  port: 8082,
+});
+
+// 들어오는 문자를 수신
+
+socket.on('connection', (ws, req) => {
+  ws.on('message', (msg) => {
+    console.log('유저가 보낸거: ' + msg);
+    ws.send('나는 서버');
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import KeyDetector from './util/KeyDetector';
 
 function App() {
+  const [value, setValue] = useState();
+
+  let socket = new WebSocket('ws://localhost:8082');
+  console.log(socket);
+
+  socket.onopen = function (e) {
+    console.log(
+      '[open] 커넥션이 만들어졌습니다. 데이터를 서버에 전송해봅시다.'
+    );
+    socket.send('My name is Bora');
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      alert(
+        `[close] 커넥션이 정상적으로 종료되었습니다(code=${event.code} reason=${event.reason})`
+      );
+    } else {
+      // 예시: 프로세스가 죽거나 네트워크에 장애가 있는 경우
+      // event.code가 1006이 됩니다.
+      alert('[close] 커넥션이 죽었습니다.');
+    }
+  };
+
+  socket.onerror = function (error) {
+    alert(`[error]`);
+  };
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setValue(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    socket.onmessage = (res) => {
+      console.log(res.data);
+    };
+    console.log(value);
+    socket.send(`${value}`);
+  };
+
+  const buttonRef = useRef();
+
+  const handleKeyDetector = (keyValue) => {
+    if (keyValue === 'Enter') {
+      buttonRef.current.click();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button ref={buttonRef} onClick={handleClick}>
+        클릭
+      </button>
+
+      <input value={value} onChange={handleChange}></input>
+
+      <KeyDetector sendKeyValue={handleKeyDetector} />
     </div>
   );
 }
